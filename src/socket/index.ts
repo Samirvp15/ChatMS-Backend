b6bf9a8096a9cf935da7128326cd71bc1f25baf4
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2025-02-19 17:53:25 -0500

    add getPreviousMessages

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -46,6 +46,16 @@ io.on("connection", async (socket) => {
         }
 
         socket.emit('message-user', payload)
+
+        //get previous message
+        const getConversationMessage = await ConversationModel.findOne({
+            "$or": [
+                { sender: user?._id, receiver: userId },
+                { sender: userId, receiver: user?._id }
+            ]
+        }).populate('messages').sort({ updatedAt: -1 })
+
+        socket.emit('message', getConversationMessage?.messages || [])
     })
 
     type CombinedType = MessageType & ConversationType;
